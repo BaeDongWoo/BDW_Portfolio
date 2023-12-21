f28157f9430dee3462dd8acf73ef0b6546f043aa
BaeDongWoo <ehddn5410@naver.com>
2023-12-21 15:55:24 +0900

    feat:add components

--- src/components/pages/page2/index.tsx
+++ src/components/pages/page2/index.tsx
@@ -1,18 +1,45 @@
 import { RefObject } from 'react';
 import styled from 'styled-components';
+import ProfileImg from './profile';
+import Info from './infomation';
+
 export type AboutMeProps = {
   element: RefObject<HTMLDivElement>;
 };
 const AboutMe = ({ element }: AboutMeProps) => {
   return (
     <Container ref={element}>
-      <h1>AboutMe</h1>;
+      <UserContainer>
+        <ProfileImg />
+        <Info />
+      </UserContainer>
     </Container>
   );
 };
 const Container = styled.div`
-  background-color: pink;
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
+`;
+const UserContainer = styled.div`
+  display: flex;
+  margin: auto;
+  margin-top: 50px;
+  background-color: white;
+  border-radius: 25px;
+  box-shadow: 8px 4px 8px rgba(0, 0, 0, 0.5);
+  width: 90vw;
+  height: 90vh;
+  max-width: 1570px;
+  max-height: 720px;
+  flex-wrap: wrap;
+  overflow: scroll;
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  @media (max-width: 1023px) {
+    flex-direction: column;
+  }
 `;
 export default AboutMe;
