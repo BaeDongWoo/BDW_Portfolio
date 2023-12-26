92c2b55a3617df471f74905854624684a02f969d
BaeDongWoo <ehddn5410@naver.com>
2023-12-26 23:22:26 +0900

    feat:add skillIconData

---
+++ src/components/data/skillIconData.js
@@ -0,0 +1,47 @@
+export const SKILL_ICON_LIST = [
+  {
+    name: 'react',
+    bgcol: '#61DAFB',
+    color: '#fff',
+  },
+  {
+    name: 'js',
+    bgcol: '#F7DF1E',
+    color: '#fff',
+  },
+  {
+    name: 'nodejs',
+    bgcol: '#339933',
+    color: '#fff',
+  },
+  {
+    name: 'nextjs',
+    bgcol: '#000000',
+    color: '#fff',
+  },
+  {
+    name: 'html',
+    bgcol: '#E34F26',
+    color: '#fff',
+  },
+  {
+    name: 'css',
+    bgcol: '#1572B6',
+    color: '#fff',
+  },
+  {
+    name: 'ts',
+    bgcol: '#3178C6',
+    color: '#fff',
+  },
+  {
+    name: 'springboot',
+    bgcol: '#6DB33F',
+    color: '#fff',
+  },
+  {
+    name: 'mysql',
+    bgcol: '#4479A1',
+    color: '#fff',
+  },
+];
