d12074f7a7a51ca73d16c756af19ee936b055cb6
BaeDongWoo <ehddn5410@naver.com>
2023-12-29 02:34:46 +0900

    feat:add content data props

--- src/components/data/skillIconData.js
+++ src/components/data/skillIconData.js
@@ -1,47 +1,82 @@
 export const SKILL_ICON_LIST = [
   {
-    name: 'react',
+    name: 'REACT',
     bgcol: '#61DAFB',
     color: '#fff',
+    content: `컴포넌트 기반의 코드 작성이 가능합니다.
+    useState, useEffect 등의 훅을 사용할 수 있고, 
+    필요에 따라 커스텀 훅을 직접 만들어 사용했던 경험이 있습니다.
+    Redux를 이용한 상태 관리 라이브러리를 활용하여 복잡한 
+    애플리케이션의 상태를 효과적으로 관리한 경험이 있습니다.
+    Next.js를 활용하여 서버 사이드 렌더링(SSR)을 구현한 경험이 있습니다.
+    리액트에서 주로 사용되는 스타일링 방법 중 하나인 
+    styled-components를 활용하여 컴포넌트 스타일을 관리했던 경험이 있습니다.`,
   },
   {
-    name: 'js',
+    name: 'JS',
     bgcol: '#F7DF1E',
     color: '#fff',
+    content: `ES6+ 문법에 익숙합니다.
+    jest를 활용한 테스트코드 작성 경험이 있습니다.
+    다양한 라이브러리의 활용이 가능합니다.
+    github page를 활용한 빌드 및 배포 경험이 있습니다.`,
   },
   {
-    name: 'nodejs',
-    bgcol: '#339933',
-    color: '#fff',
-  },
-  {
-    name: 'nextjs',
-    bgcol: '#000000',
-    color: '#fff',
-  },
-  {
-    name: 'html',
-    bgcol: '#E34F26',
-    color: '#fff',
-  },
-  {
-    name: 'css',
-    bgcol: '#1572B6',
-    color: '#fff',
-  },
-  {
-    name: 'ts',
+    name: 'TS',
     bgcol: '#3178C6',
     color: '#fff',
+    content: `정적 타입 언어인 TypeScript를 사용함으로써 안정성을 확보하고, 
+    인터페이스와 타입을 정의하여 코드를 더 명확하고 확장 가능하게 만들 수 있습니다. 
+    `,
   },
   {
-    name: 'springboot',
+    name: 'HTML',
+    bgcol: '#E34F26',
+    color: '#fff',
+    content: `HTML 태그 및 구조를 이해하고 시멘틱 태그를 사용하여 
+    웹 문서의 구조와 의미를 더 명확하게 
+    전달할 수 있습니다.`,
+  },
+  {
+    name: 'CSS',
+    bgcol: '#1572B6',
+    color: '#fff',
+    content: `기본적인 스타일링, 선택자, 박스 모델, 레이아웃 등을 이해하고 있으며, 
+    미디어 쿼리를 사용한 반응형 웹화면에 대응할 수 있습니다.
+    keyframe을 활용한 애니메이션 기법을 활용할 수 있습니다. 
+    SCSS 문법을 사용할 수 있고, styled-components를 사용해 컴포넌트 별 스타일링이 가능하며, 
+    props를 활용한 동적인 스타일링을 할 수 있습니다.`,
+  },
+  {
+    name: 'NODE.JS',
+    bgcol: '#339933',
+    color: '#fff',
+    content: `Express를 활용해 백엔드 서버를 개발한 경험이 있습니다. 
+    Node.js와 함께 사용하여 뛰어난 성능과 확장성을 제공하는 Express를 통해 
+    RESTful API를 설계하고 구현했습니다. 또한, 데이터베이스와 연동하여 데이터를 관리했던 경험이 있습니다.
+    `,
+  },
+  {
+    name: 'NEXT.JS',
+    bgcol: '#000000',
+    color: '#fff',
+    content: `app 라우팅 방식과 page라우팅 방식의 차이를 이해하고 있습니다.
+    서버 사이드 렌더링(SSR) 및 검색 엔진 최적화(SEO)와 관련된 개선 사항을 파악하고 적용할 수 있습니다.`,
+  },
+
+  {
+    name: 'SpringBoot',
     bgcol: '#6DB33F',
     color: '#fff',
+    content: `기본적인 Java 문법을 사용할 수 있고, 
+    MVC 패턴의 백엔드 서버를 개발했던 경험이 있습니다.`,
   },
   {
-    name: 'mysql',
+    name: 'MYSQL',
     bgcol: '#4479A1',
     color: '#fff',
+    content: `기본적인 SQL문법을 이해하고 사용 가능합니다.
+    ERD_Cloud를 통해 직접 다이어그램을 작성하고 데이터 베이스를 설계했던 경험이 있습니다.
+    `,
   },
 ];
