1b374e7e5d9fa981916c2fa9bacee8b8deda3ab7
BaeDongWoo <ehddn5410@naver.com>
2024-01-03 04:08:30 +0900

    feat:add projectData file

---
+++ src/components/data/projectData.js
@@ -0,0 +1,47 @@
+export const PROJECT_DATA = [
+  {
+    front: {
+      img: '123.jpg',
+      title: '포트폴리오',
+      people: '1인 프로젝트',
+      discript: '시부어',
+    },
+    back: {
+      mainFunc: ['하이', '바보', '입니다.'],
+      url: [{ urlTitle: '깃허브', url: 'https://github.com/BaeDongWoo' }],
+      skills: [
+        { name: 'REACT', bgcol: '#61DAFB', color: '#fff' },
+        { name: 'TS', bgcol: '#3178C6', color: '#fff' },
+        { name: 'NEXT.JS', bgcol: '#000000', color: '#fff' },
+      ],
+    },
+  },
+  {
+    front: {
+      img: 'My-Todo-01.png',
+      title: 'My-ToDo',
+      people: '1인 프로젝트',
+      discript: `처음 리액트를 학습할 때 간단하게 했었던 투두리스트를 생각하며, 캘린더와 드래그 앤 드롭 등 직접 구현해 보고 싶었던 기능들과 함께 학습을 위해 개발한 일정관리 서비스 입니다. 
+      
+      작은 서비스지만 처음부터 끝까지 혼자서 개발을 해본 것만으로도 의미가 있지만, 그 과정에서 Redux Store, JWT 등을 학습한 것에도 의미가 있었습니다. 또한 gitpage를 통한 배포도 경험해 볼 수 있었습니다. `,
+    },
+    back: {
+      mainFunc: ['캘린더를 통해 일정 등록', '등록한 일정 캘린더에 미리보기'],
+      url: [{ urlTitle: '', url: '' }],
+      skills: [{ name: 'REACT', bgcol: '#61DAFB', color: '#fff' }],
+    },
+  },
+  {
+    front: {
+      img: 'Clink-002.png',
+      title: '땡그랑',
+      people: '4인 프로젝트',
+      discript: '',
+    },
+    back: {
+      mainFunc: [],
+      url: [{ urlTitle: '', url: '' }],
+      skills: [{ name: 'REACT', bgcol: '#61DAFB', color: '#fff' }],
+    },
+  },
+];
