038ba68e20eeadb101fcd9d35d8bdc7aeda06563
BaeDongWoo <ehddn5410@naver.com>
2024-01-03 04:05:38 +0900

    design:add media query css

--- src/components/pages/page4/projectCard.tsx
+++ src/components/pages/page4/projectCard.tsx
@@ -2,44 +2,50 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Front from './front';
 import Back from './back';
+export interface frontDataType {
+  img: string;
+  title: string;
+  discript: string;
+  people: string;
+}
+export interface backDataType {
+  mainFunc: string[];
+  url: { urlTitle: string; url: string }[];
+  skills: { name: string; bgcol: string; color: string }[];
+}
 interface cardDataType {
   data: {
-    front: {
-      img: string;
-      title: string;
-      discript: string;
-    };
-    back: {
-      mainFunc: string[];
-      url: { urlTitle: string; url: string }[];
-      stack: { name: string; bgcol: string; color: string }[];
-    };
+    front: frontDataType;
+    back: backDataType;
   };
 }
 const ProjectCard = ({ data }: cardDataType) => {
   const [isFlipped, setIsFlipped] = useState<string>('true');
-
   const handleFlip = () => {
     setIsFlipped(isFlipped === 'true' ? 'false' : 'true');
   };
   return (
-    <CardContainer onClick={handleFlip} flipped={isFlipped}>
-      <Front />
-      <Back />
+    <CardContainer flipped={isFlipped}>
+      <Front frontData={data.front} handleFlip={handleFlip} />
+      <Back backData={data.back} handleFlip={handleFlip} />
     </CardContainer>
   );
 };
 const CardContainer = styled.div<{ flipped: string }>`
-  width: 300px;
-  height: 500px;
+  width: 450px;
+  height: 700px;
   border: 1px solid #eee;
   border-radius: 25px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
   perspective: 1000px;
   transform-style: preserve-3d;
   transition: transform 0.6s;
+  @media (max-width: 767px) {
+    width: 300px;
+    height: 500px;
+  }
   transform: ${(props) =>
-    props.flipped === 'true' ? 'none' : 'rotateY(180deg)'};
+    props.flipped === 'true' ? 'none' : 'rotateY(-180deg)'};
 `;
 
 export default ProjectCard;
