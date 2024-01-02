0320f421e395c5f5552fe6a42c8eb053243d3456
BaeDongWoo <ehddn5410@naver.com>
2024-01-03 04:03:40 +0900

    feat:add Swiper, props data,click btn

--- src/components/pages/page4/back.tsx
+++ src/components/pages/page4/back.tsx
@@ -1,20 +1,63 @@
 import styled from 'styled-components';
-
-const Back = () => {
+import { backDataType } from './projectCard';
+import SkillIconBox from './iconList';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+interface propsType {
+  backData: backDataType;
+  handleFlip: () => void;
+}
+const Back = ({ backData, handleFlip }: propsType) => {
   return (
     <BackContainer>
+      <Swiper
+        pagination={{
+          type: 'fraction',
+        }}
+        navigation
+        modules={[Pagination, Navigation]}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        loop={true}
+        className="backSwiper"
+      >
+        <SwiperSlide>
+          <img src="clink-002.png"></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="clink-002.png"></img>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="clink-002.png"></img>
+        </SwiperSlide>
+      </Swiper>
       <MainFunc>
-        <h3>주요기술</h3>
+        <h3>주요기능</h3>
         <ul>
-          <li>기능1</li>
-          <li>기능2</li>
-          <li>기능3</li>
+          {backData.mainFunc.map((func, index) => (
+            <li key={index}>{func}</li>
+          ))}
         </ul>
       </MainFunc>
       <Location>
-        <span>깃허브:https://github.com/BaeDongWoo</span>
+        {backData.url.map((url, index) => (
+          <span key={index}>
+            {url.urlTitle} : {url.url}
+          </span>
+        ))}
       </Location>
-      <Skills>리액트</Skills>
+      <Skills>
+        {backData.skills.map((skill, index) => (
+          <SkillIconBox skill={skill} key={index} />
+        ))}
+      </Skills>
+
+      <div className="cta-reverse" onClick={handleFlip}>
+        <svg width="13px" height="10px" viewBox="0 0 13 10">
+          <path d="M1,5 L11,5"></path>
+          <polyline points="4 1 0 5 4 9"></polyline>
+        </svg>
+        <span>Click</span>
+      </div>
     </BackContainer>
   );
 };
@@ -31,9 +74,15 @@ const BackContainer = styled.div`
 `;
 const MainFunc = styled.div`
   width: 100%;
+  height: 20%;
 `;
 const Location = styled.div`
+  font-size: 0.8rem;
   width: 100%;
+  height: 20%;
 `;
-const Skills = styled.div``;
+const Skills = styled.div`
+  display: flex;
+  height: 15%;
+`;
 export default Back;
