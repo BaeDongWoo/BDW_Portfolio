a96b5619cfb166072c6aa3f2cd300059a398c501
BaeDongWoo <ehddn5410@naver.com>
2024-01-04 06:43:15 +0900

    feat:add skillIcon component

---
+++ src/components/pages/page3/icon.tsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { SkillBoxDataType } from './index';
+import { SkillIconsProps } from './skillIcons';
+interface IconPropsType extends SkillIconsProps {
+  skill: SkillBoxDataType;
+}
+const SkillIcon = ({ skill, setSelectIcon, selectIcon }: IconPropsType) => {
+  const IconHandler = (skill: SkillBoxDataType) => {
+    setSelectIcon(skill);
+  };
+  return (
+    <Icon
+      data={skill}
+      className={skill.name === selectIcon.name ? 'select' : ''}
+      onClick={() => IconHandler(skill)}
+    >
+      {skill.name}
+    </Icon>
+  );
+};
+const Icon = styled.div<{ data: SkillBoxDataType }>`
+  width: 100px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: ${(props) => props.data.bgcol};
+  color: ${(props) => props.data.color};
+  font-weight: bold;
+  box-shadow: 0px 10px 0px 0px rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  &.select {
+    box-shadow: 0px 0px 0px 5px ${(props) => props.data.bgcol}80;
+    margin-top: 15px;
+    margin-bottom: 5px;
+  }
+  &:hover {
+    margin-top: 15px;
+    margin-bottom: 5px;
+    box-shadow: 0px 0px 0px 0px;
+  }
+  @media (max-width: 1023px) {
+    width: 80px;
+    height: 30px;
+    border-radius: 15px;
+  }
+`;
+export default SkillIcon;
--- src/components/pages/page3/skillIcons.tsx
+++ src/components/pages/page3/skillIcons.tsx
@@ -2,26 +2,23 @@ import styled from 'styled-components';
 import { SKILL_ICON_LIST } from '../../data/skillIconData';
 import { Dispatch, SetStateAction } from 'react';
 import { SkillBoxDataType } from './index';
-interface SkillIconsProps {
+import SkillIcon from './icon';
+export interface SkillIconsProps {
   setSelectIcon: Dispatch<SetStateAction<SkillBoxDataType>>;
   selectIcon: SkillBoxDataType;
 }
 const SkillIcons = ({ setSelectIcon, selectIcon }: SkillIconsProps) => {
   const data: SkillBoxDataType[] = SKILL_ICON_LIST;
-  const IconHandler = (skill: SkillBoxDataType) => {
-    setSelectIcon(skill);
-  };
+
   return (
     <Container>
       {data.map((skill, index) => (
-        <Icon
+        <SkillIcon
           key={index}
-          data={skill}
-          className={skill.name === selectIcon.name ? 'select' : ''}
-          onClick={() => IconHandler(skill)}
-        >
-          {skill.name}
-        </Icon>
+          skill={skill}
+          setSelectIcon={setSelectIcon}
+          selectIcon={selectIcon}
+        />
       ))}
     </Container>
   );
@@ -35,35 +32,6 @@ const Container = styled.div`
   @media (max-width: 1023px) {
     flex-wrap: wrap;
     justify-content: center;
-  }
-`;
-const Icon = styled.div<{ data: SkillBoxDataType }>`
-  width: 100px;
-  height: 40px;
-  border-radius: 20px;
-  background-color: ${(props) => props.data.bgcol};
-  color: ${(props) => props.data.color};
-  font-weight: bold;
-  box-shadow: 0px 10px 0px 0px rgba(0, 0, 0, 0.5);
-  border: 2px solid ${(props) => props.data.bgcol};
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  &.select {
-    box-shadow: 0px 0px 0px 0px;
-    margin-top: 15px;
-    margin-bottom: 5px;
-  }
-  &:hover {
-    margin-top: 15px;
-    margin-bottom: 5px;
-    box-shadow: 0px 0px 0px 0px;
-  }
-  @media (max-width: 1023px) {
-    width: 80px;
-    height: 30px;
-    border-radius: 15px;
   }
 `;
 export default SkillIcons;
