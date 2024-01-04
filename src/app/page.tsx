f5498c93241ebfe32aae79b613e085f5bc6d5f95
BaeDongWoo <ehddn5410@naver.com>
2024-01-04 17:00:35 +0900

    feat:modify favicon, metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,12 @@ import MainPage from '@/components/pages/page1';
 import AboutMe from '@/components/pages/page2';
 import SkillPage from '@/components/pages/page3';
 import Project from '@/components/pages/page4';
-import Moon from '../app/assets/Moon.svg';
-import Sun from '../app/assets/Sun.svg';
 import AOS from 'aos';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useMoveScroll from '@/components/common/hooks/moveScroll';
 import Navbox from '@/components/common/header/burgerMenu/navbox';
 export default function Home() {
-  const [mode, setMode] = useState<Boolean>(false);
   const [mainScr, onMoveToMain] = useMoveScroll();
   const [aboutMeScr, onMoveToAboutMe] = useMoveScroll();
   const [skillScr, onMoveToSkill] = useMoveScroll();
@@ -27,40 +24,11 @@ export default function Home() {
   useEffect(() => {
     AOS.init();
   });
-
-  // useEffect(() => {
-  //   let page = 0;
-  //   const lastPage = 3;
-  //   window.addEventListener(
-  //     'wheel',
-  //     (e) => {
-  //       e.preventDefault();
-  //       if (e.deltaY > 0) {
-  //         page++;
-  //       } else if (e.deltaY < 0) {
-  //         page--;
-  //       }
-  //       if (page < 0) {
-  //         page = 0;
-  //       } else if (page > lastPage) {
-  //         page = lastPage;
-  //       }
-  //       scrolls[page]();
-  //     },
-  //     { passive: false }
-  //   );
-  // }, []);
-
   return (
     <Container>
       <Navbar scrolls={scrolls} />
       <Navbox scrolls={scrolls} />
       <Logo onClick={onMoveToMain} />
-      {/* {!mode ? (
-        <Moon width={'4rem'} height={'4rem'} onClick={() => setMode(!mode)} />
-      ) : (
-        <Sun width={'4rem'} height={'4rem'} onClick={() => setMode(!mode)} />
-      )} */}
       <MainPage element={mainScr} />
       <AboutMe element={aboutMeScr} />
       <SkillPage element={skillScr} />
