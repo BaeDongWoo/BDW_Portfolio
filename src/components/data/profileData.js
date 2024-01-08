f2b59c24e59c5b62fd3051edbd5c8859300c4ca5
BaeDongWoo <ehddn5410@naver.com>
2024-01-08 22:17:07 +0900

    feat:add linkIcon

---
+++ src/components/data/profileData.js
@@ -0,0 +1,8 @@
+export const LINK_ICON = [
+  { label: '블로그', src: './blogIcon.png', url: 'https://velog.io/@b_d_o_o' },
+  {
+    label: '깃허브',
+    src: './github.png',
+    url: 'https://github.com/BaeDongWoo',
+  },
+];
---
+++ src/components/pages/page2/linkIcon.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { LINK_ICON } from '../../data/profileData';
+const LinkIcon = () => {
+  return (
+    <Container>
+      {LINK_ICON.map((data, idx) => (
+        <IconBox key={idx}>
+          <a href={data.url} target="_blank">
+            <img src={data.src}></img>
+          </a>
+          {data.label}
+        </IconBox>
+      ))}
+    </Container>
+  );
+};
+const Container = styled.div`
+  width: 100%;
+  max-width: 500px;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+`;
+const IconBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+  a img {
+    width: 70px;
+    height: 70px;
+  }
+`;
+export default LinkIcon;
