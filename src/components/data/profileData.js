16f8dedc7217cb593f5ae989bb46a03bf0e7c6a7
BaeDongWoo <ehddn5410@naver.com>
2024-01-09 03:59:51 +0900

    feat:update data

--- src/components/data/profileData.js
+++ src/components/data/profileData.js
@@ -6,3 +6,40 @@ export const LINK_ICON = [
     url: 'https://github.com/BaeDongWoo',
   },
 ];
+export const TIMELINE_DATA = [
+  {
+    date: '2023.10 - 2023.11',
+    title: `우테코
+    프리코스`,
+    content: `4주간 매주 주어지는 과제를 vanilla JS로 구현했습니다.
+    매주 새롭게 주어지는 요구사항에 맞춰 과제를 진행하며,
+    Jest를 활용해 테스트 코드를 작성했던 경험이 있습니다.
+    `,
+  },
+  {
+    date: '2023.02 - 2023.08',
+    title: `신한 DS
+    아카데미`,
+    content: `신한 DS에서 진행한 풀스택 교육과정입니다.
+    React,SpringBoot를 활용해 프로젝트를 진행하였고,
+    팀 프로젝트를 통해 협업 툴과 Convention등을 경험 했습니다.
+    `,
+  },
+  {
+    date: '2023.01 - 2023.02',
+    title: `삼성 DX
+    알고리즘`,
+    content: `삼성 DX에서 진행하는 알고리즘 특강입니다.
+    C++언어를 활용해 프로그래밍 문제를 풀며 알고리즘을 학습 했습니다.
+    배열,연결 리스트,Queue,Stack 등을 직접 구현했던 경험이 있습니다.
+    `,
+  },
+  {
+    date: '2023.08 - ing',
+    title: '학습 시작',
+    content: `프로그래밍의 기초 문법을 익히기 위해 시작했습니다.
+    inflearn,udemy 등의 학습사이트를 통해 학습하고,
+    백준, 프로그래머스 등의 프로그래밍 문제를 풀며 학습했습니다.
+    `,
+  },
+];
