60ddc9c4c9cd7f58e5f47ca6b9423710dbd2a2a0
BaeDongWoo <ehddn5410@naver.com>
2024-01-09 04:04:41 +0900

    feat:add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from 'next';
 import './globals.css';
 export const metadata: Metadata = {
   title: '배동우 포트폴리오',
-  description: '프론트엔드 개발자를 꿈꾸는 취준생',
+  description: '프론트엔드 개발자를 꿈꾸는 배동우의 포트폴리오 입니다.',
+  keywords:
+    '배동우,포트폴리오,개발자,프론드엔드,프론트엔드 포트폴리오,프론트엔드 개발자',
   icons: {
     icon: '/favi.ico',
   },
