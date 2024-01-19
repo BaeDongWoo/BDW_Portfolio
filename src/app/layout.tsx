f3c72050a8cc943abe422fb1a2647cc8221186bc
BaeDongWoo <ehddn5410@naver.com>
2024-01-20 06:50:42 +0900

    feat:change head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Head from 'next/head';
 export const metadata: Metadata = {
   title: '배동우 | 포트폴리오',
   description: '프론트엔드 개발자를 꿈꾸는 배동우의 포트폴리오 입니다.',
@@ -18,12 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <meta
           name="google-site-verification"
           content="A7biAGh2S8sojeZl2baYrK_3ZvhvRgp8CNRLk1uzdiE"
         />
-      </Head>
+      </head>
       <meta property="og:title" content="배동우 | 포트폴리오"></meta>
       <meta
         property="og:description"
