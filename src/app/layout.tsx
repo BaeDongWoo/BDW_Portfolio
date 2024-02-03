f791a368c9f5cc763051eb68084823c76332ebc7
BaeDongWoo <ehddn5410@naver.com>
2024-02-03 20:10:50 +0900

    feat:add verification google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,26 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Head from 'next/head';
 export const metadata: Metadata = {
   title: '배동우 | 포트폴리오',
-  description: '프론트엔드 개발자를 꿈꾸는 배동우의 포트폴리오 입니다.',
+  description: '프론트엔드 개발자를 꿈꾸는 배동우의 포트폴리오입니다.',
   keywords:
-    '배동우,포트폴리오,개발자,프론드엔드,프론트엔드 포트폴리오,프론트엔드 개발자',
+    '배동우, 포트폴리오, 개발자, 프론드엔드, 프론트엔드 포트폴리오, 프론트엔드 개발자',
   icons: {
     icon: '/favi.ico',
+  },
+  verification: {
+    google: 'A7biAGh2S8sojeZl2baYrK_3ZvhvRgp8CNRLk1uzdiE',
+  },
+  openGraph: {
+    images: [
+      {
+        url: 'https://bdw-portfolio.vercel.app/BDW_01.png',
+        alt: '배동우의 포트폴리오 이미지',
+      },
+    ],
+    description: '배동우의 포트폴리오입니다.',
+    title: '배동우 | 포트폴리오',
   },
 };
 
@@ -16,20 +30,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <meta
-          name="google-site-verification"
-          content="A7biAGh2S8sojeZl2baYrK_3ZvhvRgp8CNRLk1uzdiE"
-        />
-        <meta property="og:title" content="배동우 | 포트폴리오" />
-        <meta property="og:description" content="배동우의 포트폴리오 입니다." />
-        <meta
-          property="og:image"
-          content="https://bdw-portfolio.vercel.app/BDW_01.png"
-        />
-      </head>
+    <>
       <body>{children}</body>
-    </html>
+    </>
   );
 }
